efee64268784f2304e585c9309714c63739fcca6
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-02-25 00:11:07 +0700

    Modif1_Prak6_UserController_Job3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
