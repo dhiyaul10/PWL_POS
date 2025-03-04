67e451b03cd8ccaa6aae2701f3948cb657161bea
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-03-04 23:52:37 +0700

    Modifikasi_Praktikum2.7_Job4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,6 +12,8 @@
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
@@ -20,13 +22,11 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
             <td>
-                <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | 
-                <form action="/user/hapus/{{ $d->user_id }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus user ini?')">Hapus</button>
-                </form>                
+                <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> |
+                <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
             </td>
         </tr>
         @endforeach
