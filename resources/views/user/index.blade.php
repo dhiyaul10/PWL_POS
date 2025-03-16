b2e314dbae5885e3cd952c3cc4539e71a3924187
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-03-17 03:07:41 +0700

    Modifikasi_Praktikum1_Job6

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,6 +6,7 @@
         <h3 class="card-title">{{ $page->title }}</h3> 
         <div class="card-tools"> 
           <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a> 
+          <button onclick="modalAction('{{ url('user/create/ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div> 
       </div> 
       <div class="card-body">
@@ -43,6 +44,8 @@
             </thead>
         </table>
     </div> 
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data
+backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div> 
 @endsection 
  
 @push('css') 
@@ -50,17 +53,23 @@
  
 @push('js') 
   <script> 
-    $(document).ready(function() { 
-      var dataUser = $('#table_user').DataTable({ 
-          // serverSide: true, jika ingin menggunakan server side processing 
-          serverSide: true,      
-          ajax: { 
-              "url": "{{ url('user/list') }}", 
-              "dataType": "json", 
-              "type": "POST", 
-              "data": function (d) {
-                d.level_id = $('#level_id').val();
-              }
+    // Fungsi untuk memanggil modal dengan Ajax
+    function modalAction(url = '') {
+         $('#myModal').load(url, function() {
+             $('#myModal').modal('show');
+         });
+     }
+ var dataUser;
+     $(document).ready(function() {
+         dataUser = $('#table_user').DataTable({
+             serverSide: true,  // Aktifkan server-side processing
+             ajax: {
+                 url: "{{ url('user/list') }}",  // URL untuk request data
+                 dataType: "json",
+                 type: "POST",
+                 data: function(d) {
+                     d.level_id = $('#level_id').val();  // Kirim level_id sebagai filter
+                 }
           },
           columns: [ 
             {
@@ -94,11 +103,11 @@
               searchable: false     
             } 
           ]
-        }); 
-
+        });
+      // Event ketika dropdown filter level berubah
       $('#level_id').on('change', function() {
-        dataUser.ajax.reload();
-      });
-    }); 
+             dataUser.ajax.reload();  // Reload data tabel
+         });
+     });
   </script> 
 @endpush 
