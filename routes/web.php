d9708a48528eadf3ad6c3796041f9649d320a0dc
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-04-14 13:15:49 +0700

    Modifikasi_Praktikum3_Job8

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,7 @@ Route::group(['prefix' => 'barang', 'middleware' => 'authorize:ADM,MNG'], functi
     Route::get('import', [BarangController::class, 'import']); // ajax form upload excel
     Route::post('import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
     Route::get('export_excel', [BarangController::class, 'export_excel']); // export excel
+    Route::get('export_pdf', [BarangController::class, 'export_pdf']); // export pdf
     Route::delete('/{id}',[barangController::class,'destroy']);
 });
 Route::group(['prefix'=>'stok'],function(){
