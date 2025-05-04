6fef1f28327a82f20c21b533773c2b981b3bfc96
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-05-05 02:10:56 +0700

    Modifikasi_Praktikum1_Job11

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -11,31 +11,40 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
+        //set validation
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
+            'level_id' => 'required',
+            'image' => 'required'
         ]);
 
+        //if validation fails
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
+        //create user
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            'image' => $request->image,
         ]);
-        if ($user) {
+
+        //return response JSON user is created
+        if($user) {
             return response()->json([
-                'success' => true,
-                'user' => $user,
+                'message' => 'User created successfully',
+                'data' => $user,
             ], 201);
+
+            //return JSON process insert failed
+            return response()->json([
+                'message' => 'User creation failed',
+            ], 409);
         }
-        return response()->json([
-            'success' => false,
-        ], 409);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,58 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute; // Perbaiki namespace Attribute
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Tymon\JWTAuth\Contracts\JWTSubject; // implementasi JWTSubject
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject; // implementasi JWTSubject
 
-class UserModel extends Authenticatable implements JWTSubject{
+class UserModel extends Authenticatable implements JWTSubject
+{
+    use HasFactory;
 
-    public function getJWTIdentifier()
+    /**
+     * Get the identifier that will be stored in the JWT.
+     */
+    public function getJWTIdentifier(): mixed
     {
         return $this->getKey();
     }
 
+    /**
+     * Return a key-value array, containing any custom claims to be added to the JWT.
+     */
     public function getJWTCustomClaims(): array
     {
         return [];
     }
 
-    use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at', 'foto'];
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+    protected $fillable = ['username', 'nama', 'password', 'level_id', 'image'];
+    // protected $hidden = ['password']; // jangan di tampilkan saat select
+    // protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     /**
      * Relasi ke tabel level
      */
-    public function level(): BelongsTo
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
+    protected function image(): Attribute
     {
-        return $this->level->level_nama;
-    }
-
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole(string $role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
-
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-        return $this->level->level_kode;
+        return Attribute::make(
+            get: fn ($image) => url('/storage/post/' . $image),
+        );
     }
 }
